92dc9cad95d00c096dde00f7ab6e7b0e9a0e795a
Ariel Behar <ariel.behar@hotmail.com>
2024-03-29 08:09:44 +0200

    Implementing a basic NavSocial component structure

--- components/Nav/NavSocial.tsx
+++ components/Nav/NavSocial.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import FacebookIcon from '../Icons/FacebookIcon'
+import InstagramIcon from '../Icons/InstagramIcon'
 
 function NavSocial() {
-  return (
-    <div>NavSocial</div>
-  )
+	return (
+		<div className='px-4 grid grid-cols-2'>
+			<div>
+				<h5>Get In Touch</h5>
+				<div className='flex flex-row justify-around'>
+					<p>Viber</p>
+					<p>WatsApp</p>
+				</div>
+			</div>
+			<div className='flex flex-col justify-between'>
+				<FacebookIcon />
+				<InstagramIcon />
+			</div>
+		</div>
+	)
 }
 
 export default NavSocial
